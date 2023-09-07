e342c0c6c6dc78ff8c3f32cca5b6da89427fb9d1
Alexandre Andrade <alexbdla@gmail.com>
2023-09-07 18:50:46 -0300

    Navbar styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/js/src/collapse';
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md bg-primary main-nav">
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
         <div className="container-fluid">
           <a href="link" className="nav-logo-text">
             <h4>DS Catalog</h4>
